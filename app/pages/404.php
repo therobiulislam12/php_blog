e84d22246d05ffcdca47f7f721074f1076b08fb3
Robiul Islam <therobiulislam12@gmail.com>
2023-10-23 14:19:13 +0600

    Not Found Page added

--- app/pages/404.php
+++ app/pages/404.php
@@ -1,3 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo '<h1>Page No found</h1>';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found</title>
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center py-5">
+            <div class="not-found-content">
+                <h1>Page Not Found</h1>
+                <a href="/blog/public"><button class="btn btn-primary">Go Back Home</button></a>
+                <img src="assets/images/404.jpeg" alt="" class="img-thumbnail mt-4">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
